0b6c5f681da1e47efb45bc46eb591c0d3e092c92
root <brycegolamco@gmail.com>
2018-08-26 15:16:15 -0700

    Added biases. Should update the bias now...

--- MachineLearning.py
+++ MachineLearning.py
@@ -14,8 +14,8 @@ class NeuralNetwork:
         self.numOutput = numberOutput
         self.weightIH = np.asarray(np.matlib.rand(self.numHidden,self.numInput),dtype=self.dtype)
         self.weightHO = np.asarray(np.matlib.rand(self.numOutput,self.numHidden), dtype=self.dtype)
-        self.biasH = 0#np.asarray(np.matlib.ones((self.numHidden,1)), dtype = self.dtype)
-        self.biasO =  0#np.asarray(np.matlib.ones((self.numOutput,1)), dtype = self.dtype)
+        self.biasH = np.asarray(np.matlib.ones((self.numHidden,1)), dtype = self.dtype)
+        self.biasO =  np.asarray(np.matlib.ones((self.numOutput,1)), dtype = self.dtype)
         return
 
     def feedforward(self, input):
@@ -38,44 +38,25 @@ class NeuralNetwork:
         guess = self.feedforward(input)
         #Backpropagate from Output to Hidden Layer
 
-        # propagatedWeights = np.array([],dtype=self.dtype).reshape(0,self.numOutput)
-        # HO_weights = np.array([],dtype=self.dtype).reshape(0,self.numHidden)
-        # IH_weights = np.array([],dtype=self.dtype).reshape(0, self.numInput)
-
         weight = output - guess
         weight = np.multiply(weight, np.multiply(self.outputs, (1 - self.outputs)))
         propagatedWeights = weight
+        propagatedBias = weight
         weight = np.multiply(self.hiddenInputs, weight.T).T
         self.weightHO += weight
 
         propagatedWeights = np.multiply(propagatedWeights,self.weightHO)
         propagatedWeights = np.array([np.sum(propagatedWeights,axis=0)])
-        # for i in range(0,self.numOutput):
-        #     propagatedWeights = np.concatenate((propagatedWeights,weight),axis=0)
-        #     weight = np.multiply(weight, self.hiddenInputs)
-        #     weight = weight.T
 
-        #     HO_weights = np.concatenate((HO_weights,weight),axis=0)
 
-        #self.biasO += np.multiply(output - guess, np.multiply(self.outputs, (1-self.outputs)))
-        #self.weightHO -= HO_weights
-        # tempWeight = np.array([np.sum(HO_weights,axis=0)]).T
-        # propagatedWeights = np.multiply(tempWeight,propagatedWeights)
-        # print( "prop: " + str(propagatedWeights))
+        self.biasO += propagatedBias
+
         weight = np.multiply(propagatedWeights.T,np.multiply(self.hiddenInputs, (1-self.hiddenInputs)))
+        propagatedBias = weight
         weight = np.dot(input,weight.T).T
         self.weightIH += weight
         
-        # errorsHO = propagatedWeights
-        # for i in range(0,len(errorsHO)):
-        #     weight = (errorsHO[i])
-        #     weight = np.multiply(weight, np.multiply(self.hiddenInputs[i].T, (1 - self.hiddenInputs[i].T)))
-        #     weight = np.multiply(weight, input)
-        #     weight = weight.T
-        #     IH_weights = np.concatenate((IH_weights,weight),axis=0)
-
-        # self.biasH += np.multiply(np.multiply((1- self.hiddenInputs), self.hiddenInputs), errorsHO)
-        # self.weightIH -= IH_weights
+        self.biasH += propagatedBias
         return
 
     def sigmoid(self, input):
@@ -85,50 +66,6 @@ class NeuralNetwork:
             array[i] = 1/(1+e)
         return array
 
-
-class Perceptron:
-    weights = []
-    length = 0
-    def __init__(self, length):
-        self.length = length
-        for i in range(0,self.length):
-            self.weights.append(random.uniform(-1,1))        
-        return
-
-
-    def guess(self, inputs):
-        sum = 0
-
-        print("weights are " + str(self.weights))
-        print("inputs are " + str(inputs))
-        for i in range(0,len(inputs)):
-            sum += inputs[i]*self.weights[i]
-        return self.sign(sum)
-
-    def sign(self,input):
-        print("Input is " + str(input))
-        if input >= 0:
-            return 1
-        return -1
-
-    def train(self, inputs, target):
-        guess = self.guess(inputs)
-        error = target - guess
-        cost = 0
-        for i in range(0,len(inputs)):
-            self.weights[i] += float(0.1*error*inputs[i])
-        return error
-
-    def answer(self, inputs_list):
-        targets_list = []
-        for i in inputs_list:
-            if i[0] - i[1] <= 1:
-                targets_list.append(1)
-            else:
-                targets_list.append(-1)
-        return targets_list
-
-
 data = np.array([[1],[0]])
 answer = np.array([[0]])
 
@@ -137,10 +74,10 @@ x = NeuralNetwork(2,9,1)
 for i in range(0,50000):
     #print(x.weightIH)
     #x.train(np.array([[1],[1]]),np.array([[0],[0]]))
-    x.train(np.array([[1],[0]]),np.array([[1]]))
-    x.train(np.array([[0],[1]]),np.array([[1]]))
-    x.train(np.array([[1],[1]]),np.array([[0]]))
-    x.train(np.array([[0],[0]]),np.array([[0]]))
+    x.train(np.array([[1],[0]]),np.array([[0.89]]))
+    x.train(np.array([[0],[1]]),np.array([[0.48]]))
+    x.train(np.array([[1],[1]]),np.array([[0.22]]))
+    x.train(np.array([[0],[0]]),np.array([[0.11]]))
     print("IH weights: " + str(x.weightIH))
     print("HO weights: " + str(x.weightHO))
     print("Bias H: " + str(x.biasH))
